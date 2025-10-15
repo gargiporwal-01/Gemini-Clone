30d65d4aa95c985cfcbf2266e6132fcbb38b0907
Gargi Porwal <gargi.porwal.2828@gmail.com>
2025-10-15 15:03:49 +0530

    Creating Sidebar

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Main.css"
+
+const Main = () => {
+  return (
+    <div className='main'>
+      <div className="name">
+
+      </div>
+    </div>
+  )
+}
+
+export default Main
